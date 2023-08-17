985964b9cc5ba384796224c01718209ab14209bd
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-18 00:45:24 +0300

    api: adding new person working

--- phonebook/step5/index.js
+++ phonebook/step5/index.js
@@ -8,87 +8,83 @@ let persons = [
         "id": 1,
         "name": "Arto Hellas", 
         "number": "040-123456"
-      },
-      { 
+    },
+    { 
         "id": 2,
         "name": "Ada Lovelace", 
         "number": "39-44-5323523"
-      },
-      { 
+    },
+    { 
         "id": 3,
         "name": "Dan Abramov", 
         "number": "12-43-234345"
-      },
-      { 
+    },
+    { 
         "id": 4,
         "name": "Mary Poppendieck", 
         "number": "39-23-6423122"
-      }
+    }
 ]
 
-//retrieving the persons array contents
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-//single person details
-app.get('/api/persons/:id', (request, response) =>{
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
 
-  if(person){
-    response.json(person)
-  }else{
-    response.status(404).end()
-  }
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
-//auto-generate id for the newly added person
 const generateId = () => {
-  const maxId = persons.length > 0
-  ? Math.max(...persons.map(n => n.id))
-  : 0
-  return maxId + 1
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
 }
 
-//adding a new person's info
 app.post('/api/persons', (request, response) => {
-  const body = request.body
+    const body = request.body
 
-  if(!body.content){
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
 
-  const person = {
-    content: body.content,
-    id: generateId(),
-  }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
 
-  persons = persons.concat(person)
+    persons = persons.concat(person)
 
-  response.json(note)
+    response.json(person) 
 })
 
-//displaying the time the request was received
 app.get('/info', (request, response) => {
-    const currenTime = new Date().toString()
+    const currentTime = new Date().toString()
     const numEntries = persons.length
 
     const infoDetails = `
     <div>
     <p>Phonebook has info for ${numEntries} people</p>
-    <p>${currenTime}</p>
+    <p>${currentTime}</p>
     </div>
     `
     response.send(infoDetails)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(2004).end()
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end() // Corrected status code to 204
 })
 
 const PORT = 3001
